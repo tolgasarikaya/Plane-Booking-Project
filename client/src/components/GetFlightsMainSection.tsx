3a361727f9a82a6af38e1e72e72e63b10fbbf4c0
Tolga SARIKAYA <tolga.sari.kaya@hotmail.com>
2024-08-13 07:18:15 +0300

    get flights updated

--- client/src/components/GetFlightsMainSection.tsx
+++ client/src/components/GetFlightsMainSection.tsx
@@ -6,78 +6,84 @@ import { FlightData } from "../interfaces/interfaces";
 const GetFlightsMainSection = ({ data }: { data: FlightData[] }) => {
   return (
     <div className="mt-5 w-[70%] min-w-[70%]">
-      {data.map((fligth, i) => (
-        <div
-          key={i}
-          className="relative mx-5 mb-14 w-full rounded-xl rounded-bl-none bg-white p-5"
-        >
-          <h2 className="font-bold">
-            {fligth.flightDirection === "A"
-              ? `${fligth.route.destinations[0]} - AMS`
-              : `AMS - ${fligth.route.destinations[0]}`}
-          </h2>
-          <div className="my-5 flex flex-row items-center gap-20">
-            <div>
-              <div className="flex flex-row gap-2 text-sm font-semibold text-gray-500">
-                <Icon className="" fill="#6b7280" type="plane-takeoff" />
-                Departure
-              </div>
+      {data.length > 0 ? (
+        data.map((fligth, i) => (
+          <div
+            key={i}
+            className="relative mx-5 mb-14 w-full rounded-xl rounded-bl-none bg-white p-5"
+          >
+            <h2 className="font-bold">
+              {fligth.flightDirection === "A"
+                ? `${fligth.route.destinations[0]} - AMS`
+                : `AMS - ${fligth.route.destinations[0]}`}
+            </h2>
+            <div className="my-5 flex flex-row items-center gap-20">
               <div>
-                <p className="text-xl font-bold">
-                  {formatTimestampToTime(fligth.scheduleDateTime)}
-                </p>
-                <p className="font-semibold text-slate-500">
-                  Airport:{" "}
-                  {fligth.flightDirection === "A"
-                    ? `${fligth.route.destinations[0]}`
-                    : `AMS`}
-                </p>
+                <div className="flex flex-row gap-2 text-sm font-semibold text-gray-500">
+                  <Icon className="" fill="#6b7280" type="plane-takeoff" />
+                  Departure
+                </div>
+                <div>
+                  <p className="text-xl font-bold">
+                    {formatTimestampToTime(fligth.scheduleDateTime)}
+                  </p>
+                  <p className="font-semibold text-slate-500">
+                    Airport:{" "}
+                    {fligth.flightDirection === "A"
+                      ? `${fligth.route.destinations[0]}`
+                      : `AMS`}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="flex-grow border-b-2 border-black"></div>
-            <div className="flex flex-col items-center">
-              <Icon className="size-8" type="plane" fill="#4a0097" />
-              <div className="font-semibold text-slate-500">
-                3h 30m (Nonstop)
+              <div className="flex-grow border-b-2 border-black"></div>
+              <div className="flex flex-col items-center">
+                <Icon className="size-8" type="plane" fill="#4a0097" />
+                <div className="font-semibold text-slate-500">
+                  3h 30m (Nonstop)
+                </div>
               </div>
-            </div>
-            <div className="flex-grow border-b-2 border-black"></div>
-            <div>
-              <div className="flex flex-row gap-2 text-sm font-semibold text-gray-500">
-                <Icon className="" fill="#6b7280" type="plane-land" />
-                Arrival
-              </div>
+              <div className="flex-grow border-b-2 border-black"></div>
               <div>
-                <p className="text-xl font-bold">
-                  {formatTimestampToTime(
-                    addTimeToDate(fligth.scheduleDateTime),
-                  )}
-                </p>
-                <p className="font-semibold text-slate-500">
-                  Airport:{" "}
-                  {fligth.flightDirection === "A"
-                    ? `AMS`
-                    : `${fligth.route.destinations[0]}`}
-                </p>
+                <div className="flex flex-row gap-2 text-sm font-semibold text-gray-500">
+                  <Icon className="" fill="#6b7280" type="plane-land" />
+                  Arrival
+                </div>
+                <div>
+                  <p className="text-xl font-bold">
+                    {formatTimestampToTime(
+                      addTimeToDate(fligth.scheduleDateTime),
+                    )}
+                  </p>
+                  <p className="font-semibold text-slate-500">
+                    Airport:{" "}
+                    {fligth.flightDirection === "A"
+                      ? `AMS`
+                      : `${fligth.route.destinations[0]}`}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex flex-row justify-between">
-            <div>
-              <p className="text-xl font-bold text-[#4a0097]">
-                Price: ${Number(Math.random().toFixed(2)) * 100 + 200}
-              </p>
-              <p className="font-semibold text-slate-500">Round Trip</p>
+            <div className="flex flex-row justify-between">
+              <div>
+                <p className="text-xl font-bold text-[#4a0097]">
+                  Price: ${Number(Math.random().toFixed(2)) * 100 + 200}
+                </p>
+                <p className="font-semibold text-slate-500">Round Trip</p>
+              </div>
             </div>
+            <button className="absolute bottom-0 right-0 w-1/5 rounded-br-xl rounded-tl-xl bg-[#4a0097] py-6 font-semibold text-[#f6f4f8]">
+              Book Flight
+            </button>
+            <button className="absolute -bottom-10 left-0 rounded-b-xl bg-[#e6e0eb] px-5 py-2 font-semibold text-[#4a0097] hover:underline">
+              Check the details
+            </button>
           </div>
-          <button className="absolute bottom-0 right-0 w-1/5 rounded-br-xl rounded-tl-xl bg-[#4a0097] py-6 font-semibold text-[#f6f4f8]">
-            Book Flight
-          </button>
-          <button className="absolute -bottom-10 left-0 rounded-b-xl bg-[#e6e0eb] px-5 py-2 font-semibold text-[#4a0097] hover:underline">
-            Check the details
-          </button>
+        ))
+      ) : (
+        <div className="mx-5 mb-14 h-[228px] w-full rounded-xl rounded-bl-none bg-white p-5 text-slate-500">
+          No data to show.
         </div>
-      ))}
+      )}
     </div>
   );
 };
---
+++ client/src/components/OtherTopics.tsx
@@ -0,0 +1,38 @@
+import Icon from "../assets/icons/Icon";
+import carRental from "../assets/images/car-rentals.webp";
+import hostels from "../assets/images/hostels.webp";
+import travelPackage from "../assets/images/travel-package.webp";
+
+const OtherTopics = () => {
+  return (
+    <div className="w-1/4 px-8">
+      <div className="relative mt-5 cursor-pointer overflow-hidden rounded-3xl border-2 transition-all duration-300 ease-in-out hover:scale-110">
+        <img src={carRental} alt="rent a car" className="h-[200px] w-[280px]" />
+        <div className="absolute bottom-3 left-3">
+          <Icon type="car" className="size-12" fill="white" />
+          <p className="font-bold text-white">CAR RENTALS</p>
+        </div>
+      </div>
+      <div className="relative mt-5 cursor-pointer overflow-hidden rounded-3xl border-2 transition-all duration-300 ease-in-out hover:scale-110">
+        <img src={hostels} alt="hostels" className="h-[200px] w-[280px]" />
+        <div className="absolute bottom-3 left-3">
+          <Icon type="hotel" className="size-12" fill="white" />
+          <p className="font-bold text-white">HOTELS</p>
+        </div>
+      </div>
+      <div className="relative mt-5 cursor-pointer overflow-hidden rounded-3xl border-2 transition-all duration-300 ease-in-out hover:scale-110">
+        <img
+          src={travelPackage}
+          alt="travel package"
+          className="h-[200px] w-[280px]"
+        />
+        <div className="absolute bottom-3 left-3">
+          <Icon type="travel" className="size-12" fill="white" />
+          <p className="font-bold text-white">TRAVEL PACKAGE</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OtherTopics;
